1a1dcf0f7008d9365816641ffdb3121632591608
Ivaylo <ivaylo@starcoders.com>
2025-11-03 15:26:21 +0200

    more style fixes

--- src/components/Gallery/ParticipantsList/ParticipantsList.tsx
+++ src/components/Gallery/ParticipantsList/ParticipantsList.tsx
@@ -5,41 +5,47 @@ import classes from "./ParticipantsList.module.css";
 import type { Author } from "../Galleries";
 
 type Props = {
-  participants: Author[];
-  allParticipantsLength: number;
-  edition: string;
-  lang?: string;
-  onAuthorNavigate?: (authorSlug: string) => void;
+    participants: Author[];
+    allParticipantsLength: number;
+    edition: string;
+    lang?: string;
+    onAuthorNavigate?: (authorSlug: string) => void;
 };
 
-const ParticipantsList: React.FC<Props> = ({ participants, edition, lang = "en", onAuthorNavigate }) => {
-  const participantsHTML = participants.map((author) => {
-    const slug = author.name.replace(/ /g, "_");
-    const handleNavigate = () => {
-      if (typeof window !== "undefined") {
-        localStorage.setItem("hasHistory", "1");
-      }
-      onAuthorNavigate?.(slug);
-    };
-    return author.urls.length ? (
-      <div className={`col-md-6 ${classes.singleImg}`} key={`${author.name}_${edition}`}>
-        <div className={classes.imgContainer} id={author.name.replace(/ /g, "_")}> 
-          <Link onClick={handleNavigate} href={`/${lang}/editions/${edition}/${slug}`}>
-            <img src={`/${author.urlsMedium[0]}`} alt="" />
-          </Link>
-        </div>
-        <Link onClick={handleNavigate} href={`/${lang}/editions/${edition}/${slug}`}>
-          <p className={classes.authorName}>{author.name}</p>
-        </Link>
-        <p className={classes.authorCountry}>{author.country}</p>
-      </div>
-    ) : null;
-  });
-  return (
-    <div className="container">
-      <div className="row">{participantsHTML}</div>
-    </div>
-  );
+const ParticipantsList: React.FC<Props> = ({
+                                               participants,
+                                               edition,
+                                               lang = "en",
+                                               onAuthorNavigate,
+                                           }) => {
+    const participantsHTML = participants.map((author) => {
+        const slug = author.name.replace(/ /g, "_");
+        const handleNavigate = () => {
+            if (typeof window !== "undefined") {
+                localStorage.setItem("hasHistory", "1");
+            }
+            onAuthorNavigate?.(slug);
+        };
+
+        return author.urls.length ? (
+            <div className={classes.singleImg} key={`${author.name}_${edition}`}>
+                <div
+                    className={classes.imgContainer}
+                    id={author.name.replace(/ /g, "_")}
+                >
+                    <Link onClick={handleNavigate} href={`/${lang}/editions/${edition}/${slug}`}>
+                        <img src={`/${author.urlsMedium[0]}`} alt={author.name} />
+                    </Link>
+                </div>
+                <Link onClick={handleNavigate} href={`/${lang}/editions/${edition}/${slug}`}>
+                    <p className={classes.authorName}>{author.name}</p>
+                </Link>
+                <p className={classes.authorCountry}>{author.country}</p>
+            </div>
+        ) : null;
+    });
+
+    return <div className={classes.participantsGrid}>{participantsHTML}</div>;
 };
 
 export default ParticipantsList;
